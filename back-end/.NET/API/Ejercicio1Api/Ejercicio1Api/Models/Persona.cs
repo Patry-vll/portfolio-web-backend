5617e0cad3e8f70286cc84f674f9e6958e5f55f2
Patry-vll <patryka.vll@gmail.com>
2025-08-04 12:43:45 +0200

    feat: Crear modelo de Persona

---
+++ back-end/.NET/API/Ejercicio1Api/Ejercicio1Api/Models/Persona.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio1Api.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required int  Edad{ get; set; }
+        public required string Email{ get; set; }
+        public string? Direccion { get; set; }
+
+    }
+}
